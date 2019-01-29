8f030a6c5acfa204d981eed3e87ed5f532c170d7
Redanz <nader.zargham@gmail.com>
2019-01-29 00:14:59 -0800

    added answer options

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -128,74 +128,143 @@ var start = true;
 var storeData = true;
 var questionNum = 0;
 var forward = true;
+var allAnswers = [];
+var correctAnswers = [];
+var answersArrays = [];
 
 $.ajax({
 		url: queryURL,
 		method: "GET"
 	}).then(function(response){
 		triviaData = response.results;
+		document.querySelectorAll('.key')[0].style.visibility = 'visible';
+		document.querySelectorAll('.key')[1].style.visibility = 'visible';
+		document.querySelectorAll('.key')[2].style.visibility = 'visible';
+		document.querySelector('#loadButton').style.visibility = 'visible';
 	});
 
 
 function cleanData(str1){
-	return str1.replace(/&amp;/g,"&").replace(/&gt;/g, ">").replace(/&lt;/g, "<").replace(/&quot;/g, "\"").replace(/&#039;/g, "\'");
+	return str1.replace(/&amp;/g,"&").replace(/&gt;/g, ">").replace(/&lt;/g, "<").replace(/&quot;/g, "\"").replace(/&#039;/g, "\'").replace(/&pi;/g, "pi");
 }
 
-function cleanObjectData(obj){
+function cleanAndStoreData(obj){
 
 	keys = Object.keys(obj[0]);
 
 	// loops through original object and pushes clean version of objects to triviaDataClean
 	for (var i=0; i<obj.length; i++){
-		// has to be inside the loop: https://stackoverflow.com/questions/19054997/push-is-overwriting-previous-data-in-array
+		// var has to be declared inside the loop: https://stackoverflow.com/questions/19054997/push-is-overwriting-previous-data-in-array
 		var newCleanObj = {};
 		// loops through keys of original object array and cleans strings
 		for (var j=0; j<keys.length; j++){
 			// checks if key has multiple values (in an array)
 			if (typeof(obj[i][keys[j]]) != 'string'){
-				// loops through values in the array to clean string
+				var array = [];
+				// loops through values in the array to clean string and push to an array
 				for (var k=0; k<obj[i][keys[j]].length; k++){
-					newCleanObj[keys[j]] = cleanData(obj[i][keys[j]][k]);
+					array.push(cleanData(obj[i][keys[j]][k]));
 					
 				}
+				newCleanObj[keys[j]] = array;
 			} else {
 				newCleanObj[keys[j]] = cleanData(obj[i][keys[j]]);
-
 			}
 		}
 		triviaDataClean.push(newCleanObj);
 	}
+	storeAnswers(triviaDataClean);
 	return triviaDataClean;
 }
 
+function storeAnswers(data){
+	var ithAnswerArray = [];
+
+	for (var i=0; i<data.length; i++){
+		ithAnswerArray = [];
+		var answersObj = {};
+		var arrIncorrect = [];
+		answersObj['correct'] = data[i].correct_answer;
+
+		ithAnswerArray.push(data[i].correct_answer);
+
+		for (var j=0; j<data[i].incorrect_answers.length; j++){
+			arrIncorrect.push(data[i].incorrect_answers[j]);
+		}
+
+		ithAnswerArray.push.apply(ithAnswerArray, arrIncorrect);
+		answersObj['incorrect'] = arrIncorrect;
+		allAnswers.push(answersObj);
+
+		answersArrays.push(ithAnswerArray);
+	}
+	return allAnswers;
+}
+
+//takes an array and returns an array with same elements in different order
+function jumbleArray(arr){
+	//to preserve original array
+	var cloneArray = JSON.parse(JSON.stringify(arr));
+	len = cloneArray.length;
+	var newArray = [];
+	for (var i=0; i<len; i++){
+		randomIndex = Math.floor(Math.random()*cloneArray.length);
+		newArray.push(cloneArray[randomIndex]);
+		cloneArray.splice(randomIndex, 1);
+	}
+	return newArray;
+}
+
+function displayAnswerOptions(pageNum){
+	index = pageNum-1;
+	var answers = jumbleArray(answersArrays[index]);
+		if (triviaDataClean[index].type == 'multiple'){
+			document.querySelectorAll('.boolean')[0].style.visibility = 'visible';
+			document.querySelectorAll('.boolean')[1].style.visibility = 'visible';
+			document.querySelectorAll('.multiple')[0].style.visibility = 'visible';
+			document.querySelectorAll('.multiple')[1].style.visibility = 'visible';
+			$('#answerOption1').text(answers[0]);
+			$('#answerOption2').text(answers[1]);
+			$('#answerOption3').text(answers[2]);
+			$('#answerOption4').text(answers[3]);
+		} else {
+			document.querySelectorAll('.boolean')[0].style.visibility = 'visible';
+			document.querySelectorAll('.boolean')[1].style.visibility = 'visible';
+			document.querySelectorAll('.multiple')[0].style.visibility = 'hidden';
+			document.querySelectorAll('.multiple')[1].style.visibility = 'hidden';
+			$('#answerOption1').text('True');
+			$('#answerOption2').text('False');
+			$('#answerOption3').text('');
+			$('#answerOption4').text('');
+		}	
+}
 
 function displayValuesForPage(pageNum){
-
-	$('#category').text(triviaDataClean[(pageNum-1)].category);
-	$('#question').text(triviaDataClean[(pageNum-1)].question);
+	index = pageNum-1;
+	$('#category').text(triviaDataClean[(index)].category);
+	$('#type').text(triviaDataClean[(index)].type);
+	$('#question').text(triviaDataClean[(index)].question);
 	$('#questionNumber').text(' ' + (pageNum));
 	$('#loadButton').text('Next Question');
 	start=false;
+	
 	if (questionNum == 10){
-
 		$('#loadButton').text('Submit!');
 
 	} else if (questionNum > 1){
-
 		document.querySelector('#previousButton').style.visibility = 'visible';
 	
 	} else {
-
 		document.querySelector('#previousButton').style.visibility = 'hidden';
 	}
+	displayAnswerOptions(pageNum);
 }
-
 
 $('#loadButton').on('click', function(){
 
 	if (storeData == true){
 		questionNum++;
-		cleanObjectData(triviaData);
+		cleanAndStoreData(triviaData);
 		displayValuesForPage(questionNum);
 		storeData = false;
 
@@ -207,12 +276,10 @@ $('#loadButton').on('click', function(){
 			questionNum++;
 			displayValuesForPage(questionNum);
 	}
-
 })
 
 
 $('#previousButton').on('click', function(){
-
 	questionNum--;
 	displayValuesForPage(questionNum);
 })
